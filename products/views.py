4358817e085a655a3288d80bfddd9ef8c3e43a2f
felipeortizs <jfortizs@eafit.edu.co>
2023-08-29 17:14:09 -0500

    Views del sign in, html del sign in y base html con barra de navegacion

--- products/views.py
+++ products/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class SingIn(View):
+    template_name = "products/singin.html"
+
+    def get(self, request):
+        viewData = {
+            "form": AuthenticationForm
+        } 
+        return render(request, self.template_name, viewData)
+
+    def post(self, request):
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            viewData = {
+            "form": AuthenticationForm,
+            "message": 'Usuario o contraseña incorrecto '
+            } 
+            return render(request, self.template_name, viewData)
+        else:
+            login(request,user)
+            return redirect('home')
