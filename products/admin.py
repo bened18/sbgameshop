5f06d4a1e14436ff7f99151698dc8b7244ce5d8f
Bened <belt@eafit.edu.co>
2023-08-30 12:21:39 -0500

    agregando categoria, genero y plataforma a los modelos

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product
+from .models import Product, Category, Genre, Platform
 
 admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Platform)
--- products/models.py
+++ products/models.py
@@ -1,18 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    category = models.CharField(max_length=50)
-    platform = models.CharField(max_length=50)
-    description = models.TextField()
-    price = models.FloatField()
-    image = models.ImageField(upload_to="product_images/", default="product_images\The-Witcher-3-Wild-Hunt.jpg")
+# Modelo para categoria de los productos
+class Category(models.Model):
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
+# Modelo para generos de videojuegos
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+# Modelo para plataformas del producto (PC, PlayStation, Xbox, Nintendo, etc.)
+class Platform(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+# Main product model
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.FloatField()
+    image = models.ImageField(upload_to="product_images/", default="product_images\The-Witcher-3-Wild-Hunt.jpg")
+    genres = models.ManyToManyField(Genre, blank=True)  # For video game genres
+
+    def __str__(self):
+        return self.name
+
+# Model for user shopping carts
 class ShoppingCart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, through='CartItem')
@@ -21,7 +44,8 @@ class ShoppingCart(models.Model):
     def calculate_total_cost(self):
         self.total_cost = sum(item.subtotal for item in self.cart_items.all())
         self.save()
-    
+
+# Model for cart items
 class CartItem(models.Model):
     cart = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE, related_name='cart_items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
