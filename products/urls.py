f1acb900d93716c6c569e159d7a85cda5fe877a4
Bened <belt@eafit.edu.co>
2023-09-10 10:39:07 -0500

    comentando el codigo

--- products/urls.py
+++ products/urls.py
@@ -7,13 +7,12 @@ from .views import DeleteCartItem, SearchResultsView, FilterResultsView
 urlpatterns = [
     path('',ProductView.as_view(), name='home'),
     path('search/', SearchResultsView.as_view(), name='search_results'),
-    path('filter/', FilterResultsView.as_view(), name='filter_results'),
-    path('singupform/', SingUpView.as_view(), name='singupform'),
-    path('singup/', SingUpView.as_view(), name='singup'),
     path('product/<int:product_id>/', ProductDetail.as_view(), name='product_detail'),
-    path('add_to_cart/<int:product_id>/', AddToCart.as_view(), name='add_to_cart'),
-    path('singout/', SingOut.as_view(), name='singout'),
+    path('filter/', FilterResultsView.as_view(), name='filter_results'),
     path('singin/', SingIn.as_view(), name='singin'),
+    path('singupform/', SingUpView.as_view(), name='singupform'),
+    path('singout/', SingOut.as_view(), name='singout'),
+    path('add_to_cart/<int:product_id>/', AddToCart.as_view(), name='add_to_cart'),
     path('view_cart/', CartView.as_view(), name='view_cart'),
     path('delete_cart_item/<int:cart_item_id>', DeleteCartItem.as_view(), name='delete_cart_item'),
 ]
--- products/views.py
+++ products/views.py
@@ -13,77 +13,111 @@ from django.utils.decorators import method_decorator
 class SingUpView(TemplateView): 
     template_name = "products/singup.html"
 
+    #Si el metodo es 'get' se envia el formulario de creación de usuario
     def get(self, request):
         viewData = {
+            #formulario de creación de usuario con la clase 'django.contrib.auth.forms'
             "form": UserCreationForm
         } 
         return render(request, self.template_name, viewData)
 
+    #si el metodo es 'post' se valida la creación del usuario
     def post(self, request):
+        #Validamos que las contraseñas coincidan
         if request.POST['password1'] == request.POST['password2']:
+            #tratamos de registrar al usuario en la base de datos
             try:
-                #registrar usuario
+                #registrar usuario enviando el 'username' y la 'password'
                 user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                #guardamos el usuario en la base de datos
                 user.save()
+                #iniciamos sesion automaticamente
                 login(request, user)
+                #redirigimos a la url de home
                 return redirect('home')
+            #si no pudimos registrar al usuario es por que ya existe en la base de datos
             except IntegrityError:
                 viewData = {
+                    #volvemos a enviar el formulario
                     "form": UserCreationForm,
+                    #enviamos un mensaje de error
                     "message": "el usuario ya existe"
                 } 
                 return render(request, self.template_name, viewData)
-        viewData = {
-                "form": UserCreationForm,
-                "message": "las contraseñas no son iguales"
-            } 
-        return render(request, self.template_name, viewData)
+        #si las contraseñas no son iguales le pedimos que vuelva a intentar
+        else:
+            viewData = {
+                     #volvemos a enviar el formulario
+                    "form": UserCreationForm,
+                    #enviamos un mensaje de error
+                    "message": "las contraseñas no son iguales"
+                } 
+            return render(request, self.template_name, viewData)
 
 class SingOut(View):
     def get(self, request):
+        #Se cierra sesion
         logout(request)
         return redirect('home')
 
 class SingIn(View):
     template_name = "products/singin.html"
 
+    #si el metodo es 'get' se envia el formulario para iniciar sesion
     def get(self, request):
         viewData = {
+            #formulario de inicio de sesion con la clase 'django.contrib.auth.forms'
             "form": AuthenticationForm
         } 
         return render(request, self.template_name, viewData)
     
+    #si el metodo es 'post' se valida el formulario
     def post(self, request):
+        #se verifica que el usuario coincida con la base de datos
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        #si el usuario no existe
         if user is None:
             viewData = {
+            #se vueve a enviar el form
             "form": AuthenticationForm,
-            "message": 'Usuario o contraseña incorrecto '
+            #se envia mensaje de error
+            "message": 'Usuario o contraseña incorrecto'
             } 
             return render(request, self.template_name, viewData)
+        #si el usuario si existe
         else:
+            #se inicia sesion
             login(request,user)
             return redirect('home')
 
 class ProductView(TemplateView): 
     template_name = "products/products.html"
 
+    #Mostrar todos los productos de la base de datos
     def get(self, request): 
         viewData = {
-            "products": Product.objects.all()
-        } 
+            #Obtenemos todos los productos de la base de datos
+            "products": Product.objects.all() 
+        }
+        #Enviamos todos los productos a products.html para ser mostrados
         return render(request, self.template_name, viewData)
 
 class SearchResultsView(View):
     template_name = "products/search_results.html"
 
+    #Mostrar el producto buscado por la barra de busqueda
     def get(self, request):
+        #Obtenemos el string de lo que se esta buscando
         query = request.GET.get('query', '')
+        #Filtramos por productos que contengan el query
         products = Product.objects.filter(name__icontains=query)
         viewData = {
+            #Enviamos los productos que coincidan
             "products": products,
-            "query": query  # Pasamos el término de búsqueda a la plantilla
+            # Enviamos el string de búsqueda a la plantilla
+            "query": query
         }
+        #se envia el diccionario viewData a 'search_results.html'
         return render(request, self.template_name, viewData)
 
 class FilterResultsView(TemplateView): 
